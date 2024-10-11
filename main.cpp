398adc83c7012485d32a41c687b75858c1d7b50c
Raksa <OCraksa@gmail.com>
2024-10-11 14:20:28 +0700

    commit practice

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> number;
+    number = {1, 2, 4, 5, 6, 7, 2, 2, 5, 67, 4, 32, 42, 34};
+    size_t arrSize = number.size();
+    int *p = &number[0];
+    for (size_t i = 0; i < arrSize; i++)
+    {
+        cout << *p++ << endl;
+    }
+    return 0;
+}
